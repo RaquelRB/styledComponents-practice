bdc48d4a7a025178d08a68eaf5fb17a746c2b04d
RaquelRB <raquelrobles95@gmail.com>
2021-03-05 20:10:47 +0100

    styled-practice

---
+++ src/components/Products/data.js
@@ -0,0 +1,117 @@
+
+import product1 from '../../images/product-1.jpg'
+import product2 from '../../images/product-2.jpg'
+import product3 from '../../images/product-3.jpg'
+import product4 from '../../images/product-4.jpg'
+import product5 from '../../images/product-5.jpg'
+import product6 from '../../images/product-6.jpg'
+import sweet1 from '../../images/sweet-1.jpg'
+import sweet2 from '../../images/sweet-2.jpg'
+import sweet3 from '../../images/sweet-3.jpg'
+import sweet4 from '../../images/sweet-4.jpg'
+import sweet5 from '../../images/sweet-5.jpg'
+import sweet6 from '../../images/sweet-6.jpg'
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+    {
+        img: product4,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+    {
+        img: product5,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+    {
+        img: product6,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+
+];
+
+export const productDataTwo = [
+    {
+        img: sweet1,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet2,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet3,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet4,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet5,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet6,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '20€',
+        button: 'Add to Cart'
+    },
+
+]
